f41e6d2e8813c3e32e1ff000ad0f516425a3ae7d
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-11-27 17:10:13 -0500

    Fix GitGuardian incident

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # makereddit
- A Make School clone of Reddit
+
+A Make School clone of Reddit
 
 ![sample room](public/images/screenshot.png)
 
 ## How to Run
+
 1. [Clone](https://github.com/Abhiek187/makereddit.git) this repository.
-2. `npm install`
-3. `npm start`
-4. Go to `localhost:3000`.
+2. Create a `.env` file with the following content, where `YOUR_USERNAME` and `YOUR_PASSWORD` are your MongoDB credentials:
+
+```
+MONGO_USERNAME=YOUR_USERNAME
+MONGO_PASSWORD=YOUR_PASSWORD
+```
+
+3. `npm install`
+4. `npm start`
+5. Go to `localhost:3000`.
--- app.js
+++ app.js
@@ -1,56 +1,66 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const rooms = require('./routes/rooms');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const rooms = require("./routes/rooms");
 
 var app = express();
 
 // configure sessions
-const session = require('express-session');
-app.use(session({ secret: 'secret-unique-code', cookie: { maxAge: 3600000 }, resave: true, saveUninitialized: true }));
+const session = require("express-session");
+app.use(
+  session({
+    secret: "secret-unique-code",
+    cookie: { maxAge: 3600000 },
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/rooms', rooms);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/rooms", rooms);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // Database setup
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://ms-user:makeschool@cluster0-n7ctu.mongodb.net/test?retryWrites=true&w=majority';
+const mongoose = require("mongoose");
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const mongoURI = `mongodb+srv://${username}:${password}@cluster0-n7ctu.mongodb.net/test?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 module.exports = app;
